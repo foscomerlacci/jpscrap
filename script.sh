beafea47d6e1dd5a6917381295c7734e27aeb9aa
foscomerlacci <foscomerlacci@tiscali.it>
2017-06-25 01:05:30 +0200

    i primi 3 script funzionanti

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 # cd /home/utente/PycharmProjects/Test/jpscrap
-./subito/subito.py > /dev/pts/3
-./kitlavoro/kitlavoro.py > /dev/pts/3
-./iprogrammatori/iprogrammatori.py > /dev/pts/3
+./subito/subito.py
+./kitlavoro/kitlavoro.py
+./iprogrammatori/iprogrammatori.py
