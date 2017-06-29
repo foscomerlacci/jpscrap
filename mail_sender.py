e4692e3854137e61f9802f2a21abf113965ef2c2
foscomerlacci <foscomerlacci@tiscali.it>
2017-06-29 15:12:13 +0200

    ripulito mail sender

--- mail_sender.py
+++ mail_sender.py
@@ -1,19 +1,11 @@
 import smtplib
-import sys
-# to_addr_list  = ['pit0sf0r0w@gmail.com']
-# cc_addr_list = ['example@gmail.com']
-# subject      = 'Howdy'
-# # message      =  sys.argv[0]
-# login        = 'pit0sf0r0w@gmail.com'
-# password     = 'aa718vsaa718vs'
-# from_addr    = ['jpscrap']
 
 
-def sendemail(from_addr= ['alert@jpscr.ap'],
+def sendemail(from_addr= 'alert@jpscr.ap',
               to_addr_list=['pit0sf0r0w@gmail.com'],
               cc_addr_list=['bgtube@yahoo.it'],
               subject= 'Cercasi SISTEMISTA',
-              message='',
+              message='thtf',
               login='pit0sf0r0w@gmail.com',
               password='aa718vsaa718vs',
               smtpserver='smtp.gmail.com',
@@ -27,18 +19,8 @@ def sendemail(from_addr= ['alert@jpscr.ap'],
     server = smtplib.SMTP(smtpserver, smtpport)  # use both smtpserver  and -port
     server.starttls()
     server.login(login,password)
-    problems = server.sendmail(from_addr, to_addr_list, message)
+    server.sendmail(from_addr, to_addr_list, message)
+    # problems = server.sendmail(from_addr, to_addr_list, message)
     server.quit()
 
-# sendemail(from_addr    = 'python@RC.net',
-#         to_addr_list = ['pit0sf0r0w@gmail.com'],
-#         cc_addr_list = ['bgtube@yahoo.it'],
-#         subject      = 'Cercasi SISTEMISTA',
-#         # message      = sys.argv[1],
-#         message      = sys.argv[0],
-#         #                 lo cercano lo cercano lo cercano... \n
-#         #                 e alla fine hanno trovato  http://www.kitlavoro.it/lavoro/3727022/sistemista-linux-roma-roma-provincia/''',
-#         # login        = login,
-#         login        = 'pit0sf0r0w@gmail.com',
-#         password     = 'aa718vsaa718vs')
 
